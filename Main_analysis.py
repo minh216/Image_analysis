9a79bfc72c6eb7459ea114c8546931566df0c7e5
minh216 <mnhdao22@gmail.com>
2026-01-30 14:29:57 +1100

    define function to extract green channel

--- Main_analysis.py
+++ Main_analysis.py
@@ -45,8 +45,8 @@ full_path = file_path / json_name # full path to the JSON file the / operator jo
 
 
 
-def extract_image_exposure_pairs(json_name, file_path):
-    """Extract image names and shutter speeds as list of tuples"""
+def extract_image_Information(json_name, file_path):
+    """Extract image names, file paths cr2, file path tif and shutter speeds as list of tuples"""
     full_path = file_path / json_name
     with open(full_path, 'r') as json_file:
         metadata = json.load(json_file)
@@ -54,12 +54,33 @@ def extract_image_exposure_pairs(json_name, file_path):
         
         Image_info = [
             (capture.get('image_name', '').replace('.CR2', '.tif'),
+             file_path / capture.get('image_name', ''),
+             file_path / capture.get('image_name', '').replace('.CR2', '.tif'),
              capture.get('camera_settings', {}).get('shutter_speed'))
             for capture in captures
         ]
         
         return Image_info
 
-# run the function to extract image names and shutter speeds as list of tuples
-Image_info = extract_image_exposure_pairs(json_name, file_path)  
-print(f"Extracted image names and shutter speeds:{Image_info}")
+# # run the function to extract image names and shutter speeds as list of tuples
+image_info = extract_image_Information(json_name, file_path)  
+# print(f"Extracted image names and shutter speeds:{image_info}")
+
+# Define a function that extract just the green channel from the tif image_path 
+
+def extract_green_channel(image_path):
+    image = tifffile.imread(image_path)  # read the tif image
+    green_channel = image[:, :, 1]  # extract the green channel
+    return green_channel    
+
+# # Test the function on the first image info tuple
+green_channel_test = extract_green_channel(image_info[0][2])
+# # Display the green channel
+plt.imshow(green_channel_test, cmap='gray')
+plt.show()
+
+print(f"Data type: {green_channel_test.dtype}")
+print(f"Shape: {green_channel_test.shape}")
+print(f"Min value: {green_channel_test.min()}")
+print(f"Max value: {green_channel_test.max()}")
+# print({image_info[0]})
